a56b6946210ee656ff57d7f67d21eaada8259bc2
Yota Toyama <raviqqe@gmail.com>
2025-07-13 21:11:09 -0700

    Fix

--- main.go
+++ main.go
@@ -22,41 +22,46 @@ func init() {
 	godog.BindCommandLineFlags("", &options)
 }
 
+func before(ctx context.Context, _ *godog.Scenario) (context.Context, error) {
+	d, err := os.MkdirTemp("", "godog-*")
+
+	return context.WithValue(ctx, "directory", d), err
+}
+
+func createFile(ctx context.Context, p string) error {
+	return os.WriteFile(path.Join(ctx.Value("directory").(string), p), nil, 0o644)
+}
+
+func runCommand(ctx context.Context, successfully, command string) (context.Context, error) {
+	ss := strings.Split(command, " ")
+	c := exec.Command(ss[0], ss[1:]...)
+	c.Dir = ctx.Value("directory").(string)
+
+	err := c.Run()
+	ctx = context.WithValue(ctx, "exitCode", c.ProcessState.ExitCode())
+
+	if successfully == "" {
+		return ctx, nil
+	}
+
+	return ctx, err
+}
+
+func exitStatus(ctx context.Context, not string, code int) error {
+	c := ctx.Value("exitCode").(int)
+
+	if (not == "") == (c == code) {
+		return nil
+	}
+
+	return fmt.Errorf("expected exit code %s%d but got %d", not, code, c)
+}
+
 func InitializeScenario(scenario *godog.ScenarioContext) {
-	scenario.Before(func(ctx context.Context, _ *godog.Scenario) (context.Context, error) {
-		d, err := os.MkdirTemp("", "godog-*")
-
-		return context.WithValue(ctx, "directory", d), err
-	})
-
-	scenario.Step(`^a file named "((\\\\|\\"|[^"\\])+)" with:$`, func(ctx context.Context, p string) error {
-		return os.WriteFile(path.Join(ctx.Value("directory").(string), p), nil, 0o644)
-	})
-
-	scenario.Step("^I (successfully |)run `(.*)`$", func(ctx context.Context, successfully, command string) (context.Context, error) {
-		ss := strings.Split(command, " ")
-		c := exec.Command(ss[0], ss[1:]...)
-		c.Dir = ctx.Value("directory").(string)
-
-		err := c.Run()
-		ctx = context.WithValue(ctx, "exitCode", c.ProcessState.ExitCode())
-
-		if successfully == "" {
-			return ctx, nil
-		}
-
-		return ctx, err
-	})
-
-	scenario.Step(`^the exit status should (not |)be (\d+)$`, func(ctx context.Context, not string, code int) error {
-		c := ctx.Value("exitCode").(int)
-
-		if (not == "") == (c == code) {
-			return nil
-		}
-
-		return fmt.Errorf("expected exit code %s%d but got %d", not, code, c)
-	})
+	scenario.Before(before)
+	scenario.Step(`^a file named "((\\\\|\\"|[^"\\])+)" with:$`, createFile)
+	scenario.Step("^I (successfully |)run `(.*)`$", runCommand)
+	scenario.Step(`^the exit status should (not |)be (\d+)$`, exitStatus)
 }
 
 func main() {
