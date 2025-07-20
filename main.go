2e79eaec060003366de7c63c9f5a176f6f4392a3
Yota Toyama <raviqqe@gmail.com>
2025-07-19 17:03:38 -0700

    Fix

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package aruba
 
 import (
 	"bytes"
@@ -129,23 +129,10 @@ func stdout(ctx context.Context, stdout, not, exactly, expected string) error {
 	return nil
 }
 
-func initializeScenario(scenario *godog.ScenarioContext) {
+func InitializeScenario(scenario *godog.ScenarioContext) {
 	scenario.Before(before)
 	scenario.Step(`^a file named "((?:\\.|[^"\\])+)" with:$`, createFile)
 	scenario.Step("^I (successfully |)run `(.*)`$", runCommand)
 	scenario.Step(`^the exit status should (not |)be (\d+)$`, exitStatus)
 	scenario.Step(`^the (std(?:out|err)) should (not |)contain (exactly |)"((?:\\.|[^"\\])*)"$`, stdout)
-}
-
-func main() {
-	pflag.Parse()
-	options.Paths = pflag.Args()
-
-	status := godog.TestSuite{
-		Name:                "aruba",
-		ScenarioInitializer: initializeScenario,
-		Options:             &options,
-	}.Run()
-
-	os.Exit(status)
 }
