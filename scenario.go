f43b5300afd1ef365b22e3ef7e584307a26d0d53
Yota Toyama <raviqqe@gmail.com>
2025-07-20 10:14:42 -0700

    Fix

--- scenario.go
+++ scenario.go
@@ -119,7 +119,7 @@ func stdout(ctx context.Context, stdout, not, exactly, expected string) error {
 func InitializeScenario(scenario *godog.ScenarioContext) {
 	scenario.Before(before)
 	scenario.Step(`^a file named "((?:\\.|[^"\\])+)" with:$`, createFile)
-	scenario.Step("^I( successfully|) run `(.*)`$", runCommand)
+	scenario.Step("^I( successfully|) run `(.*)`( interactively)?$", runCommand)
 	scenario.Step(`^the exit status should( not|) be (\d+)$`, exitStatus)
 	scenario.Step(
 		`^the (std(?:out|err)) should( not|) contain( exactly|) "((?:\\.|[^"\\])*)"$`,
