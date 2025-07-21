01da468fcf3f1070a4161ed9e0e48cf024162752
Yota Toyama <raviqqe@gmail.com>
2025-07-20 22:39:01 -0700

    Fix

--- scenario.go
+++ scenario.go
@@ -159,15 +159,15 @@ func fileExists(ctx context.Context, ty, p, not string) error {
 // [InitializeScenario] initializes a scenario.
 func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Before(before)
-	ctx.Step(`^a file named "(.+)" with:$`, func(ctx context.Context, p string, s *godog.DocString) error {
-		return createFile(ctx, p, parseDocString(s.Content)+"\n")
-	})
 	ctx.Step(`^a file named "(.+)" with (".*")$`, func(ctx context.Context, p, s string) error {
 		s, err := parseString(s)
 		if err != nil {
 			return err
 		}
 		return createFile(ctx, p, s)
+	})
+	ctx.Step(`^a file named "(.+)" with:$`, func(ctx context.Context, p string, s *godog.DocString) error {
+		return createFile(ctx, p, parseDocString(s.Content)+"\n")
 	})
 	ctx.Step(`^a directory named "(.+)"$`, createDirectory)
 	ctx.Step("^I( successfully)? run (`.*`)( interactively)?$", runCommand)
