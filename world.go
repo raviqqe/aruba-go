f253ee1bde81149841dfb61c39b48a6f2ecab2c7
Yota Toyama <raviqqe@gmail.com>
2025-07-21 19:06:19 -0700

    Fix

--- world.go
+++ world.go
@@ -5,26 +5,65 @@ import (
 	"context"
 	"io"
 	"os/exec"
+	"strings"
 )
 
 type worldKey struct{}
 
 type world struct {
-	Command          *exec.Cmd
+	commands         []*exec.Cmd
 	RootDirectory    string
 	CurrentDirectory string
 	Stdin            io.WriteCloser
-	Stdout           *bytes.Buffer
-	Stderr           *bytes.Buffer
 }
 
 func newWorld(d string) world {
 	return world{
 		RootDirectory:    d,
 		CurrentDirectory: d,
-		Stdout:           bytes.NewBuffer(nil),
-		Stderr:           bytes.NewBuffer(nil),
 	}
+}
+
+func (w world) AddCommand(c *exec.Cmd) world {
+	w.commands = append(w.commands, c)
+	return w
+}
+
+func (w world) FindCommand(s string) *exec.Cmd {
+	for _, c := range w.commands {
+		if s == strings.Join(c.Args, " ") {
+			return c
+		}
+	}
+
+	return nil
+}
+
+func (w world) LastCommand() *exec.Cmd {
+	return w.commands[len(w.commands)-1]
+}
+
+func (w world) Stdout() string {
+	return w.stdout(func(c *exec.Cmd) io.Writer {
+		return c.Stdout
+	})
+}
+
+func (w world) Stderr() string {
+	return w.stdout(func(c *exec.Cmd) io.Writer {
+		return c.Stderr
+	})
+}
+
+func (w world) stdout(f func(*exec.Cmd) io.Writer) string {
+	bs := []byte(nil)
+
+	for _, c := range w.commands {
+		_ = c.Wait()
+		bs = append(bs, f(c).(*bytes.Buffer).Bytes()...)
+	}
+
+	return string(bs)
 }
 
 func contextWorld(ctx context.Context) world {
