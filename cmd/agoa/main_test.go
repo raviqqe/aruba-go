9e8d6b0ec792ab91fc16208d6d5a9584159814e5
Yota Toyama <raviqqe@gmail.com>
2025-07-23 08:39:41 -0700

    Fix

--- cmd/agoa/main_test.go
+++ cmd/agoa/main_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/raviqqe/aruba-go/cmd/agoa"
@@ -11,5 +12,13 @@ func TestRunNoFeature(t *testing.T) {
 	status, err := main.Run()
 
 	assert.Equal(t, 1, status)
+	assert.Equal(t, nil, err)
+}
+
+func TestRunFeatures(t *testing.T) {
+	os.Chdir("../..")
+	status, err := main.Run()
+
+	assert.Equal(t, 0, status)
 	assert.Equal(t, nil, err)
 }
